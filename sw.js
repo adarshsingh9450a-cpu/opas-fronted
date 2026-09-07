99e41772b8f471924135197cebb09e3296414284
adarshsingh9450a-cpu <adarshsingh9450a@gmail.com>
2026-09-07 21:19:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-self.addEventListener('fetch', function(event) {});
+// OPAS Offline Service Worker
+const CACHE_NAME = 'opas-app-cache-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/logo.png.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
